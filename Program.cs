3924a2cfbd8fc2e2aa5c5c1064535fc9c186270b
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-03-09 17:22:03 -0300

    ConversaoString

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string texto = "Hello, world!";
+
+char[] caracteres = texto.ToCharArray();
+int comprimento = caracteres.Length;
+
+for (int i = 0; i < comprimento / 2; i++)
+{
+    char temp = caracteres[i];
+    caracteres[i] = caracteres[comprimento - i - 1];
+    caracteres[comprimento - i - 1] = temp;
+}
+
+string textoInvertido = new string(caracteres);
+Console.WriteLine(textoInvertido);
